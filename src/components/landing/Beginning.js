9da117c11e750d3d30397e83ec9349833776360b
aramirezdecruz3148 <leigh3148@gmail.com>
2020-03-16 15:57:24 -0700

    Added a test video to the code base and set up an initial component for the landing page. All will most likely change, just beginning to play around

---
+++ src/components/landing/Beginning.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Beginning() {
+  return (
+    <div>
+      {/* this is just a test video below */}
+      <iframe src="/src/assets/testing.mp4">
+      </iframe>
+      <button>
+        Begin The Adventure
+      </button>
+    </div>
+  );
+}

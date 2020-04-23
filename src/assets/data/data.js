94591edad17823f8e2a9d0f98e02e34c7101e79e
aramirezdecruz3148 <leigh3148@gmail.com>
2020-04-23 09:45:50 -0700

    added mp4 to url loader, imported videos inside data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,39 +1,48 @@
+import start from '../videos/start.mp4';
+import beginning from '../videos/beginning.mp4';
+import middle1 from '../videos/middle1.mp4';
+import middle2 from '../videos/middle2.mp4';
+import ending1 from '../videos/ending1.mp4';
+import ending2 from '../videos/ending2.mp4';
+import ending3 from '../videos/ending3.mp4';
+import ending4 from '../videos/ending4.mp4';
+
 const data = new Map([
   ['start', {
-    src: './src/assets/videos/start.mp4',
+    src: start
   }],
   ['beginning', {
-    src: './src/assets/videos/beginning.mp4',
+    src: beginning,
     option1Text: 'HARRY',
     option2Text: 'RON',
     option1Value: 'middleA',
     option2Value: 'middleB'
   }],
   ['middleA', {
-    src: './src/assets/videos/middle1.mp4',
+    src: middle1,
     option1Text: 'FIGHT',
     option2Text: 'RUN',
     option1Value: 'endingA',
     option2Value: 'endingB'
   }],
   ['middleB', {
-    src: './src/assets/videos/middle2.mp4',
+    src: middle2,
     option1Text: 'A PEAK INSIDE',
     option2Text: 'A JOYRIDE',
     option1Value: 'endingC',
     option2Value: 'endingD'
   }],
   ['endingA', {
-    src: './src/assets/videos/ending1.mp4',
+    src: ending1,
   }],
   ['endingB', {
-    src: './src/assets/videos/ending2.mp4',
+    src: ending2,
   }],
   ['endingC', {
-    src: './src/assets/videos/ending3.mp4',
+    src: ending3,
   }],
   ['endingD', {
-    src: './src/assets/videos/ending4.mp4',
+    src: ending4,
   }]
 ]);
 

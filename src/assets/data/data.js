2148410598bb1e0e4c959110a41b061220b16ce5
aramirezdecruz3148 <leigh3148@gmail.com>
2020-04-23 08:04:49 -0700

    working to fix pathing issue for deploy

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,39 +1,39 @@
 const data = new Map([
   ['start', {
-    src: 'src/assets/videos/start.mp4',
+    src: './src/assets/videos/start.mp4',
   }],
   ['beginning', {
-    src: 'src/assets/videos/beginning.mp4',
+    src: './src/assets/videos/beginning.mp4',
     option1Text: 'HARRY',
     option2Text: 'RON',
     option1Value: 'middleA',
     option2Value: 'middleB'
   }],
   ['middleA', {
-    src: 'src/assets/videos/middle1.mp4',
+    src: './src/assets/videos/middle1.mp4',
     option1Text: 'FIGHT',
     option2Text: 'RUN',
     option1Value: 'endingA',
     option2Value: 'endingB'
   }],
   ['middleB', {
-    src: 'src/assets/videos/middle2.mp4',
+    src: './src/assets/videos/middle2.mp4',
     option1Text: 'A PEAK INSIDE',
     option2Text: 'A JOYRIDE',
     option1Value: 'endingC',
     option2Value: 'endingD'
   }],
   ['endingA', {
-    src: 'src/assets/videos/ending1.mp4',
+    src: './src/assets/videos/ending1.mp4',
   }],
   ['endingB', {
-    src: 'src/assets/videos/ending2.mp4',
+    src: './src/assets/videos/ending2.mp4',
   }],
   ['endingC', {
-    src: 'src/assets/videos/ending3.mp4',
+    src: './src/assets/videos/ending3.mp4',
   }],
   ['endingD', {
-    src: 'src/assets/videos/ending4.mp4',
+    src: './src/assets/videos/ending4.mp4',
   }]
 ]);
 
